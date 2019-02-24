1c625de01e7c4f12b33592db11228bb5c83dff05
Cam Tullos <cam@tullos.ninja>
2019-02-23 21:53:31 -0500

    Removed unnecessary op include

--- src/app/components/ReCarousel/Slide/index.js
+++ src/app/components/ReCarousel/Slide/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import cn from 'classnames';
-import op from 'object-path';
 
 /**
  * -----------------------------------------------------------------------------
